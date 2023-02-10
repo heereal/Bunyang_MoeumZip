b2b585b09788002e2afb96bba494662490a2c21f
YS739 <suk921@gmail.com>
2023-02-10 21:34:42 +0900

    test: 메인 tabs 구현중 탭을 한 번 클릭 하면 다른 탭으로 변경 안 되는 문제

--- src/components/MainPage/HomeList.tsx
+++ src/components/MainPage/HomeList.tsx
@@ -3,6 +3,7 @@ import * as S from './style';
 
 const HomeList = ({ home }: any) => {
   const router = useRouter();
+
   return (
     <S.ListWrap onClick={() => router.push(`/detail/${home.PBLANC_NO}`)}>
       <div>
@@ -18,25 +19,37 @@ const HomeList = ({ home }: any) => {
         <div>작은 사진</div>
       </div>
 
-      <div>{home.TOT_SUPLY_HSHLDCO}</div>
+      <div>
+        <div>분양세대</div>
+        {home.TOT_SUPLY_HSHLDCO} 세대
+      </div>
       {/* {home.detail.length !== 0 ? ():()} */}
       <div>
+        <div>분양면적</div>
+        {home.detail[0]?.SUPLY_AR.split('.')[0]}m2 ~
+        {home.detail[home.detail.length - 1]?.SUPLY_AR.split('.')[0]}m2
+      </div>
+      <div>
+        <div>전용면적</div>
         {/* TODO: m2 슬라이스해야함 2자리일때는 앞이 0.. */}
-        {home.detail[0]?.SUPLY_AR}m2 ~
-        {home.detail[home.detail.length - 1]?.SUPLY_AR}m2
+        {home.detail[0]?.HOUSE_TY.split('.')[0]}m2 ~
+        {home.detail[home.detail.length - 1]?.HOUSE_TY.split('.')[0]}m2
       </div>
       <div>
-        {home.detail[0]?.HOUSE_TY}m2 ~
-        {home.detail[home.detail.length - 1]?.HOUSE_TY}m2
-      </div>
-      <div>
+        <div>분양가격</div>
         {home.detail[0]?.LTTOT_TOP_AMOUNT}만원 ~
         {home.detail[home.detail.length - 1]?.LTTOT_TOP_AMOUNT}만원
       </div>
       {/*  */}
       <div>
-        <div>{home.SPSPLY_RCEPT_BGNDE}</div>
-        <div>{home.RCEPT_BGNDE}</div>
+        <div>
+          <div>특별 청약일</div>
+          {home.SPSPLY_RCEPT_BGNDE} ~ {home.SPSPLY_RCEPT_ENDDE}
+        </div>
+        <div>
+          <div>청약 접수일</div>
+          {home.RCEPT_BGNDE} ~ {home.RCEPT_ENDDE}
+        </div>
       </div>
     </S.ListWrap>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { useSubscription } from '@/hooks';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import HomeList from '@/components/MainPage/HomeList';
 import HeadTitle from '@/components/GlobalComponents/HeadTitle/HeadTitle';
 
@@ -11,12 +11,43 @@ import HeadTitle from '@/components/GlobalComponents/HeadTitle/HeadTitle';
 
 const MainPage = () => {
   const { homeListHandler, homeList } = useSubscription();
+  const [todayHome, setTodayHome] = useState<any>([]);
+  const [comingHome, setComingHome] = useState<any>([]);
 
   console.log(homeList);
 
   useEffect(() => {
     homeListHandler();
   }, []);
+
+  // 오늘 날짜 구하기
+  const postTime = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    return year + '-' + month + '-' + day;
+  };
+  const today = postTime();
+
+  const todayList = homeList.filter((item: any) => item.RCEPT_BGNDE <= today);
+
+  // 오늘 청약 Tab - 청약일이 오늘 날짜보다 작은 분양 리스트 가져오기
+  const todayListHandler = () => {
+    // TODO: item.RCEPT_BGNDE <= today && item.RCEPT_ENDDE >= today 로 해야 하는데
+    // 현재 결과가 없음
+    setTodayHome(todayList);
+  };
+
+  console.log(todayHome);
+
+  // 청약 임박 Tab(곧 청약이 시작돼요!)
+  const comingListHandler = () => {
+    const comingList = homeList.filter((item: any) => item.RCEPT_BGNDE > today);
+    setComingHome(comingList);
+  };
+
+  console.log(comingHome);
 
   return (
     <>
@@ -27,22 +58,38 @@ const MainPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
+        {/* TODO: 청약 가능? */}
         <div>오늘 청약</div>
         <div>청약 임박</div>
         <div>무순위</div>
       </div>
 
       <div>
-        <button>오늘 청약</button>
-        <button>청약 임박</button>
+        <button onClick={todayListHandler}>오늘 청약</button>
+        <button onClick={comingListHandler}>청약 임박</button>
         <button>무순위</button>
       </div>
-      <div>
-        {/* TODO: props로 넘겨주지 않고 각 컴포넌트에서 실행하기? */}
-        {homeList.map((item: any) => (
-          <HomeList key={item.PBLANC_NO} home={item} />
-        ))}
-      </div>
+      {/* TODO: props로 넘겨주지 않고 각 컴포넌트에서 실행하기? */}
+
+      {comingHome.length !== 0 ? (
+        <div>
+          {comingHome?.map((item: any) => (
+            <HomeList key={item.PBLANC_NO} home={item} />
+          ))}
+        </div>
+      ) : todayHome.length !== 0 ? (
+        <div>
+          {todayHome?.map((item: any) => (
+            <HomeList key={item.PBLANC_NO} home={item} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          {todayList?.map((item: any) => (
+            <HomeList key={item.PBLANC_NO} home={item} />
+          ))}
+        </div>
+      )}
     </>
   );
 };
