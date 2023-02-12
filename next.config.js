5485a2a2e06f2402bdbc9234b2ddceb2d17c5f46
YS739 <suk921@gmail.com>
2023-02-12 21:50:53 +0900

    edit: rewrites로 API KEY 숨기기

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
+const BASE_URL = 'https://api.odcloud.kr/api/ApplyhomeInfoDetailSvc/v1';
+const METHOD_APT_ALL = 'getAPTLttotPblancDetail';
+const METHOD_APT_DETAIL = 'getAPTLttotPblancMdl';
+const SERVICE_KEY = process.env.NEXT_PUBLIC_HOME_API_KEY;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: false,
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/default',
+        destination: `${BASE_URL}/${METHOD_APT_ALL}?page=1&perPage=1500&&cond%5BRCRIT_PBLANC_DE%3A%3AGTE%5D=2023-01-01&serviceKey=${SERVICE_KEY}`,
+      },
+      {
+        source: '/api/detail',
+        destination: `${BASE_URL}/${METHOD_APT_DETAIL}?page=1&perPage=10000&serviceKey=${SERVICE_KEY}`,
+      },
+    ];
+  },
 };
-
-const withImages = require('next-images');
-module.exports = withImages();
-
-module.exports = nextConfig;
--- src/hooks/useSubscription.tsx
+++ src/hooks/useSubscription.tsx
@@ -4,24 +4,15 @@ import { useState } from 'react';
 const useSubscription = () => {
   const [homeList, setHomeList] = useState<any>([]);
 
-  const BASE_URL = 'https://api.odcloud.kr/api/ApplyhomeInfoDetailSvc/v1';
-  const METHOD_APT_ALL = 'getAPTLttotPblancDetail';
-  const METHOD_APT_DETAIL = 'getAPTLttotPblancMdl';
-  const SERVICE_KEY = process.env.NEXT_PUBLIC_HOME_API_KEY;
-
   const homeListHandler = async () => {
     // 공고문 리스트 가져오기
     const defaultList = await axios
-      .get(
-        `${BASE_URL}/${METHOD_APT_ALL}?page=1&perPage=1500&&cond%5BRCRIT_PBLANC_DE%3A%3AGTE%5D=2023-01-01&serviceKey=${SERVICE_KEY}`,
-      )
+      .get(`/api/default`)
       .then((res) => res.data.data);
 
     // 공고문 상세정보 전체 리스트 가져오기
     const detailList = await axios
-      .get(
-        `${BASE_URL}/${METHOD_APT_DETAIL}?page=1&perPage=10000&serviceKey=${SERVICE_KEY}`,
-      )
+      .get(`/api/detail`)
       .then((res) =>
         res.data.data.filter((item: any) => item.PBLANC_NO >= 2023000000),
       );
