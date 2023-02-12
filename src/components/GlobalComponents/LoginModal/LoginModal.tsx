53a1955afb03953c382135bee909be65ee079602
heeryoung <winterbooks@naver.com>
2023-02-12 15:27:29 +0900

    feat: next-auth 패키지 설치 및 카카오 로그인 구현

--- src/components/GlobalComponents/LoginModal/LoginModal.tsx
+++ src/components/GlobalComponents/LoginModal/LoginModal.tsx
@@ -1,7 +1,13 @@
 import ReactModal from 'react-modal';
 import * as S from './style';
-import { FacebookAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import {
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from 'firebase/auth';
 import { auth } from '@/common/firebase';
+import { getCsrfToken, signIn, useSession, signOut } from 'next-auth/react';
+import { useEffect } from 'react';
 
 interface loginModalProps {
   isOpen: boolean;
@@ -12,15 +18,36 @@ const LoginModal = ({ isOpen }: loginModalProps) => {
   //FIXME: 배포 시 파베 페북 연결 ID 수정하기
   const facebookProvider = new FacebookAuthProvider();
 
+  // 유저의 세션 정보 받아오기
+  const { data: session } = useSession();
+  console.log(session);
+
   //FIXME: any 수정
   const loginHandler = (provider: any) => {
     signInWithPopup(auth, provider);
   };
+
+  // 엑세스토큰 받아오기
+  const getToken = async () => {
+    const csrfToken = await getCsrfToken();
+    console.log(csrfToken);
+  };
+
+  useEffect(() => {
+    getToken();
+  }, []);
+
   return (
     <ReactModal isOpen={isOpen} style={customStyles}>
       <S.ModalContainer>
-        <button onClick={() => loginHandler(googleProvider)}>구글 로그인</button>
-        <button onClick={() => loginHandler(facebookProvider)}>페이스북 로그인</button>
+        <button onClick={() => loginHandler(googleProvider)}>
+          구글 로그인
+        </button>
+        <button onClick={() => loginHandler(facebookProvider)}>
+          페이스북 로그인
+        </button>
+        <button onClick={() => signIn('kakao')}>카카오 로그인</button>
+        <button onClick={() => signOut()}>카카오 로그아웃</button>
       </S.ModalContainer>
     </ReactModal>
   );
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,11 @@
+import NextAuth from 'next-auth'
+import KakaoProvider from 'next-auth/providers/kakao'
+
+export default NextAuth({
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+    }),
+  ],
+})
