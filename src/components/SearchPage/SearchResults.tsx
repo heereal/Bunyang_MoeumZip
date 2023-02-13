c7a1e3d08250d98de14466e557c83de6afe89b2b
YS739 <suk921@gmail.com>
2023-02-13 21:39:54 +0900

    feat: 검색 기능 1차 구현

--- src/components/SearchPage/SearchResults.tsx
+++ src/components/SearchPage/SearchResults.tsx
@@ -1,5 +1,11 @@
-const SearchResults = () => {
-  return <div></div>;
+const SearchResults = ({ searchResult }: any) => {
+  return (
+    <div>
+      <text>아파트 이름: {searchResult.HOUSE_NM}</text>
+      <br />
+      <text>주소: {searchResult.HSSPLY_ADRES}</text>
+    </div>
+  );
 };
 
 export default SearchResults;
