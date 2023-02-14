50486cd33a79bb8ba4a1733c50a340effbbf965c
heeryoung <winterbooks@naver.com>
2023-02-14 09:36:15 +0900

    edit: 로그인 관련 함수 수정

--- src/components/GlobalComponents/LoginModal/LoginModal.tsx
+++ src/components/GlobalComponents/LoginModal/LoginModal.tsx
@@ -14,23 +14,28 @@ import {
   getDocs,
   collection,
   where,
+  addDoc,
 } from 'firebase/firestore';
+import { useState } from "react";
 
 interface loginModalProps {
   isOpen: boolean;
 }
 
 const LoginModal = ({ isOpen }: loginModalProps) => {
+  const [fireUsers, setfireUsers] = useState<any[]>([])
   const googleProvider = new GoogleAuthProvider();
   //FIXME: 배포 시 파베 페북 연결 ID 수정하기
   const facebookProvider = new FacebookAuthProvider();
 
   // 유저의 세션 정보 받아오기
   const { data: session, status } = useSession();  
-  console.log(session, status);
+  console.log(session?.user, status);
+  // console.log(fireUsers);
+  // const session2 = getSession()
 
   //FIXME: any 수정
-  const loginHandler = (provider: any) => {
+  const loginHandler2 = (provider: any) => {
     signInWithPopup(auth, provider);
   };
 
@@ -40,26 +45,52 @@ const LoginModal = ({ isOpen }: loginModalProps) => {
     console.log(csrfToken);
   };
 
-  const firebaseTest = async () => {
+  const getFirestoreUsers = async (email: string) => {
     const q = query(
       collection(db, 'Users'),
-      where('id', '==', 'a'),
+      where('id', '==', email),
     );
 
     const array: any[] = [];
     const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) =>
-      array.push({
-        id: doc.id,
-        ...doc.data(),
-      })
+      querySnapshot.forEach((doc) =>
+        array.push({
+          // id: doc.id,
+          ...doc.data(),
+        }),
       );
-      console.log(array);
-      if (array.length >= 1) {
-        console.log('잘 들어옴');
+      setfireUsers(array)
+  };
+
+  
+  const loginHandler = async (provider: string) => {
+
+    await signIn(provider)
+
+    // .then(async () => {
+      // if (!session) return;
+      // getFirestoreUsers("mika013@naver.com")
+      
+      
+      //  이미 가입했다면 바로 로그인됨
+      if (fireUsers.length >= 1) {
+        console.log('이미 가입했음')
+        
+        //처음 로그인이라면 회원가입 페이지로 이동
       } else {
-        console.log('실패');
+        const newUser = {
+          // userId: session.
+        }
+        // await addDoc(collection(db, 'Users'), session?.user);
       }
+    // })
+   
+      // console.log(array);
+      // if (array.length >= 1) {
+      //   console.log('잘 들어옴');
+      // } else {
+      //   console.log('실패');
+      // }
       
   }
  
@@ -71,14 +102,14 @@ const LoginModal = ({ isOpen }: loginModalProps) => {
   return (
     <ReactModal isOpen={isOpen} style={customStyles} ariaHideApp={false}>
       <S.ModalContainer>
-        <button onClick={() => loginHandler(googleProvider)}>
+        <button onClick={() => loginHandler2(googleProvider)}>
           구글 로그인
         </button>
-        <button onClick={() => loginHandler(facebookProvider)}>
+        <button onClick={() => loginHandler2(facebookProvider)}>
           페이스북 로그인
         </button>
-        <button onClick={() => signIn('kakao')}>카카오 로그인</button>
-        <button onClick={() => signIn('naver')}>네이버 로그인</button>
+        <button onClick={() => loginHandler('kakao')}>카카오 로그인</button>
+        <button onClick={() => loginHandler('naver')}>네이버 로그인</button>
         <button onClick={() => signOut()}>로그아웃</button>
         {session ?  <div>{session.user?.name}님 로그인 환영합니다</div> : <div>로그아웃 상태임</div>}
       </S.ModalContainer>
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,8 +8,10 @@ import {
   collection,
   where,
 } from 'firebase/firestore';
+import { useRouter } from 'next/router';
 
 export default NextAuth({
+
   session: {
     strategy: 'jwt',
   },
@@ -26,26 +28,48 @@ export default NextAuth({
       clientSecret: process.env.NAVER_CLIENT_SECRET!,
     }),
   ],
-  callbacks: {
-    async signIn({ user }) {
-      
-      const q = query(collection(db, 'Users'), where('id', '==', user.email));
-      const array: any[] = [];
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) =>
-        array.push({
-          // id: doc.id,
-          ...doc.data(),
-        }),
-      );
+//   callbacks: {
+//     async jwt(token, user, account, isNewUser) {
+//         // token = {...token.token.user}
+//         // console.log("token.name",token.name);
+//         return token;
+//     },
+//     async session(session: any, userOrToken: any) {
+//         // session.user.userId = userOrToken.userId;
+//         // session.user.test = userOrToken.test;
+//         return session
+//     }
+// }
 
-      // 이미 가입했다면 바로 로그인됨
-      if (array.length >= 1) {
-        return true;
-        //처음 로그인이라면 회원가입 페이지로 이동
-      } else {
-        return '/signup';
-      }
-    },
-  },
-});
+  callbacks: {
+  //   async signIn({ user }) {
+      
+  //     const q = query(collection(db, 'Users'), where('id', '==', user.email));
+  //     const array: any[] = [];
+  //     const querySnapshot = await getDocs(q);
+  //     querySnapshot.forEach((doc) =>
+  //       array.push({
+  //         // id: doc.id,
+  //         ...doc.data(),
+  //       }),
+  //     );
+
+  //     // 이미 가입했다면 바로 로그인됨
+  //     if (array.length >= 1) {
+  //       return true;
+  //       //처음 로그인이라면 회원가입 페이지로 이동-로그인은 안 됨
+  //     } else {
+  //       // test()
+  //       return '/signup';
+  //     }
+  //   },
+  // },
+  async redirect({ token, url, baseUrl }: any) {
+    // Allows relative callback URLs
+    // if (url.startsWith("/")) return `${baseUrl}${url}`
+    // // Allows callback URLs on the same origin
+    // else if (new URL(url).origin === baseUrl) return url
+    // return baseUrl
+  }
+  
+}});
