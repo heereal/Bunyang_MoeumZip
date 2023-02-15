0aa1785ea72acd66e1c6c7cbc4c29903e1d2759b
YS739 <suk921@gmail.com>
2023-02-15 18:59:19 +0900

    edit: firebase data넣기 시도2

--- src/pages/admin/nemo042116.tsx
+++ src/pages/admin/nemo042116.tsx
@@ -10,7 +10,7 @@ import { useState, useEffect } from 'react';
 
 const MustHaveToDo = ({ aptList, aptRandomList, officeList }: any) => {
   const queryClient = useQueryClient();
-  const [aptData, setAptData] = useState();
+  const [aptData, setAptData] = useState<any>();
 
   // console.log('aptList :>> ', aptList);
   // console.log('aptRandomList :>> ', aptRandomList);
@@ -25,55 +25,123 @@ const MustHaveToDo = ({ aptList, aptRandomList, officeList }: any) => {
   // };
   // combineData();
   // console.log(allList);
-  console.log(aptData);
 
-  useEffect(() => {
-    aptList.map((item: any) => setAptData(item));
-  }, []);
+  // const allDataList = {
+  //   SUPLY_AR: '',
+  //   HOUSE_TY: '',
+  //   LTTOT_TOP_AMOUNT: '',
+  //   SPSPLY_HSHLDCO: '',
+  //   SUPLY_HSHLDCO: '',
+  //   TOT_SUPLY_HSHLDCO: '',
+  //   HOUSE_NM: '',
+  //   HOUSE_SECD_NM: '',
+  //   HOUSE_DTL_SECD_NM: '',
+  //   HSSPLY_ADRES: '',
+  //   SUBSCRPT_AREA_CODE_NM: '',
+  //   RCEPT_BGNDE: '',
+  //   RCEPT_ENDDE: '',
+  //   SPSPLY_RCEPT_BGNDE: '',
+  //   SPSPLY_RCEPT_ENDDE: '',
 
-  // console.log(aptList((item)=>item.));
+  //   RCRIT_PBLANC_DE: '',
+  //   PRZWNER_PRESNATN_DE: '',
+  //   CNSTRCT_ENTRPS_NM: '',
+  //   BSNS_MBY_NM: '',
+  //   MDHS_TELNO: '',
+  //   CNTRCT_CNCLS_BGNDE: '',
+  //   CNTRCT_CNCLS_ENDDE: '',
+  //   MVN_PREARNGE_YM: '',
+  //   SPECLT_RDN_EARTH_AT: '',
+  //   MDAT_TRGET_AREA_SECD: '',
+  //   PBLANC_URL: '',
+  // };
+
+  // console.log(allDataList.HOUSE_NM = );
+
+  // useEffect(() => {
+  //   aptList.map((item: any) => setAptData(item));
+  // }, []);
+
+  // console.log(aptList);
+
+  // const test: any = [];
+  // const testste = aptList.map((item: any) => {
+  //   allDataList.HOUSE_NM = item.HOUSE_NM;
+  // });
+  // console.log(testste);
+
+  // SUPLY_AR: item.detail
+  // HOUSE_TY: item.HOUSE_TY,
+  // LTTOT_TOP_AMOUNT: item.LTTOT_TOP_AMOUNT,
+  // SPSPLY_HSHLDCO: item.SPSPLY_HSHLDCO,
+  // SUPLY_HSHLDCO: item.SUPLY_HSHLDCO,
+  // TOT_SUPLY_HSHLDCO: item.TOT_SUPLY_HSHLDCO,
+  // HOUSE_NM: item.HOUSE_NM,
+  // HOUSE_SECD_NM: item.HOUSE_SECD_NM,
+  // HOUSE_DTL_SECD_NM: item.HOUSE_DTL_SECD_NM,
+  // HSSPLY_ADRES: item.HSSPLY_ADRES,
+  // SUBSCRPT_AREA_CODE_NM: item.SUBSCRPT_AREA_CODE_NM,
+  // RCEPT_BGNDE: item.RCEPT_BGNDE,
+  // RCEPT_ENDDE: item.RCEPT_ENDDE,
+  // SPSPLY_RCEPT_BGNDE: item.SPSPLY_RCEPT_BGNDE,
+  // SPSPLY_RCEPT_ENDDE: item.SPSPLY_RCEPT_ENDDE,
+
+  // RCRIT_PBLANC_DE: item.RCRIT_PBLANC_DE,
+  // PRZWNER_PRESNATN_DE: item.PRZWNER_PRESNATN_DE,
+  // CNSTRCT_ENTRPS_NM: item.CNSTRCT_ENTRPS_NM,
+  // BSNS_MBY_NM: item.BSNS_MBY_NM,
+  // MDHS_TELNO: item.MDHS_TELNO,
+  // CNTRCT_CNCLS_BGNDE: item.CNTRCT_CNCLS_BGNDE,
+  // CNTRCT_CNCLS_ENDDE: item.CNTRCT_CNCLS_ENDDE,
+  // MVN_PREARNGE_YM: item.MVN_PREARNGE_YM,
+  // SPECLT_RDN_EARTH_AT: item.SPECLT_RDN_EARTH_AT,
+  // MDAT_TRGET_AREA_SECD: item.MDAT_TRGET_AREA_SECD,
+  // PBLANC_URL: item.PBLANC_URL,
 
   // 버튼 클릭 시 전체 API data가 firebase에 들어감
   const apiCallHandler = async () => {
     // aptList.map((item: any) => setAptData(item));
-    const newData = { aptData };
+    // const newData = { aptList };
 
-    // aptList.map((item: any) => {
-    //   const allList = {
-    //     SUPLY_AR: item.SUPLY_AR,
-    //     HOUSE_TY: item.HOUSE_TY,
-    //     LTTOT_TOP_AMOUNT: item.LTTOT_TOP_AMOUNT,
-    //     SPSPLY_HSHLDCO: item.SPSPLY_HSHLDCO,
-    //     SUPLY_HSHLDCO: item.SUPLY_HSHLDCO,
-    //     TOT_SUPLY_HSHLDCO: item.TOT_SUPLY_HSHLDCO,
-    //     HOUSE_NM: item.HOUSE_NM,
-    //     HOUSE_SECD_NM: item.HOUSE_SECD_NM,
-    //     HOUSE_DTL_SECD_NM: item.HOUSE_DTL_SECD_NM,
-    //     HSSPLY_ADRES: item.HSSPLY_ADRES,
-    //     SUBSCRPT_AREA_CODE_NM: item.SUBSCRPT_AREA_CODE_NM,
-    //     RCEPT_BGNDE: item.RCEPT_BGNDE,
-    //     RCEPT_ENDDE: item.RCEPT_ENDDE,
-    //     SPSPLY_RCEPT_BGNDE: item.SPSPLY_RCEPT_BGNDE,
-    //     SPSPLY_RCEPT_ENDDE: item.SPSPLY_RCEPT_ENDDE,
+    aptList.map((item: any) => {
+      const allList = [
+        {
+          // SUPLY_AR: item.SUPLY_AR,
+          // HOUSE_TY: item.HOUSE_TY,
+          // LTTOT_TOP_AMOUNT: item.LTTOT_TOP_AMOUNT,
+          // SPSPLY_HSHLDCO: item.SPSPLY_HSHLDCO,
+          // SUPLY_HSHLDCO: item.SUPLY_HSHLDCO,
+          TOT_SUPLY_HSHLDCO: item.TOT_SUPLY_HSHLDCO,
+          HOUSE_NM: item.HOUSE_NM,
+          HOUSE_SECD_NM: item.HOUSE_SECD_NM,
+          HOUSE_DTL_SECD_NM: item.HOUSE_DTL_SECD_NM,
+          HSSPLY_ADRES: item.HSSPLY_ADRES,
+          SUBSCRPT_AREA_CODE_NM: item.SUBSCRPT_AREA_CODE_NM,
+          RCEPT_BGNDE: item.RCEPT_BGNDE,
+          RCEPT_ENDDE: item.RCEPT_ENDDE,
+          SPSPLY_RCEPT_BGNDE: item.SPSPLY_RCEPT_BGNDE,
+          SPSPLY_RCEPT_ENDDE: item.SPSPLY_RCEPT_ENDDE,
 
-    //     RCRIT_PBLANC_DE: item.RCRIT_PBLANC_DE,
-    //     PRZWNER_PRESNATN_DE: item.PRZWNER_PRESNATN_DE,
-    //     CNSTRCT_ENTRPS_NM: item.CNSTRCT_ENTRPS_NM,
-    //     BSNS_MBY_NM: item.BSNS_MBY_NM,
-    //     MDHS_TELNO: item.MDHS_TELNO,
-    //     CNTRCT_CNCLS_BGNDE: item.CNTRCT_CNCLS_BGNDE,
-    //     CNTRCT_CNCLS_ENDDE: item.CNTRCT_CNCLS_ENDDE,
-    //     MVN_PREARNGE_YM: item.MVN_PREARNGE_YM,
-    //     SPECLT_RDN_EARTH_AT: item.SPECLT_RDN_EARTH_AT,
-    //     MDAT_TRGET_AREA_SECD: item.MDAT_TRGET_AREA_SECD,
-    //     PBLANC_URL: item.PBLANC_URL,
-    //   };
-    //   return allList;
-    // });
-    addHomeListMutate.mutate(newData);
+          // RCRIT_PBLANC_DE: item.RCRIT_PBLANC_DE,
+          // PRZWNER_PRESNATN_DE: item.PRZWNER_PRESNATN_DE,
+          // CNSTRCT_ENTRPS_NM: item.CNSTRCT_ENTRPS_NM,
+          // BSNS_MBY_NM: item.BSNS_MBY_NM,
+          // MDHS_TELNO: item.MDHS_TELNO,
+          // CNTRCT_CNCLS_BGNDE: item.CNTRCT_CNCLS_BGNDE,
+          // CNTRCT_CNCLS_ENDDE: item.CNTRCT_CNCLS_ENDDE,
+          // MVN_PREARNGE_YM: item.MVN_PREARNGE_YM,
+          // SPECLT_RDN_EARTH_AT: item.SPECLT_RDN_EARTH_AT,
+          // MDAT_TRGET_AREA_SECD: item.MDAT_TRGET_AREA_SECD,
+          // PBLANC_URL: item.PBLANC_URL,
+        },
+      ];
+
+      setAptData(allList);
+    });
+    addHomeListMutate.mutate(aptData);
     console.log('데이터 업로드 완료!');
   };
-
+  console.log(aptData);
   const addHomeListMutate = useMutation(addHomeList, {
     onSuccess: () => {
       queryClient.invalidateQueries('HomeList');
