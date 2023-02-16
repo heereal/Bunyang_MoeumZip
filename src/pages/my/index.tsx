6cb91a510333d98559710dfd54443dda9f06296f
heeryoung <winterbooks@naver.com>
2023-02-16 17:33:44 +0900

    feat: 닉네임 수정 기능 구현

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -2,8 +2,10 @@ import { useSession } from "next-auth/react";
 import { db } from '@/common/firebase';
 import {
   collection,
+  doc,
   getDocs,
   query,
+  updateDoc,
   where,
 } from 'firebase/firestore';
 import { useEffect, useState } from "react";
@@ -12,7 +14,10 @@ import { useRouter } from "next/router";
 const MyPage = () => {
 
   const router = useRouter();
-  const [usersList, setUsersList] = useState<any[]>([])
+  const [usersList, setUsersList] = useState<any[]>([]);
+  const [isInputOpen, setIsInputOpen] = useState(false);
+  const [nickname, setNickname] = useState<any>('');
+  const [email, setEmail] = useState<any>('');
 
   // 유저의 세션 정보 받아오기
   const { data: session, status } = useSession();
@@ -33,6 +38,22 @@ const MyPage = () => {
       }),
     );
     setUsersList(array)
+    setNickname(usersList[0]?.userName)
+    setEmail(usersList[0]?.userEmail)
+  };
+
+  // [닉네임 수정 완료] 버튼 클릭 시 작동
+  const changeNicknameHandler = async () => {
+    const checkNickname = usersList.find((user) => user.userName === nickname);
+    if (checkNickname) {
+      alert('이미 존재하는 닉네임입니다. 다시 입력해주세요.')
+      return;
+    }
+    const updateUser = {
+      userName: nickname,
+    }
+    await updateDoc(doc(db, 'Users', email), updateUser);
+    alert('닉네임 수정 완료!')
   };
 
   // 비로그인 유저일 경우 접근 제한
@@ -48,6 +69,14 @@ const MyPage = () => {
   return (
     <>
       <div>닉네임: {usersList[0]?.userName}</div>
+      <button onClick={() => setIsInputOpen(!isInputOpen)}>닉네임 수정</button>
+      {isInputOpen ? (
+        <>
+          <input value={nickname} onChange={(e) => setNickname(e.target.value)}/>
+          <button onClick={changeNicknameHandler}>닉네임 수정 완료</button>
+        </>
+      
+      ) : null}
     </>
   )
 };
