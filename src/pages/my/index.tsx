6d22a8dbbb2c9f78840ca0bf1c000cc36ff66824
heeryoung <winterbooks@naver.com>
2023-02-16 17:01:06 +0900

    feat: 로그인한 유저의 닉네임 정보 불러오기 구현

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -1,5 +1,55 @@
+import { useSession } from "next-auth/react";
+import { db } from '@/common/firebase';
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
 const MyPage = () => {
-  return <div>MyPage</div>;
+
+  const router = useRouter();
+  const [usersList, setUsersList] = useState<any[]>([])
+
+  // 유저의 세션 정보 받아오기
+  const { data: session, status } = useSession();
+
+  // firestore에서 'Users' 데이터 볼러 옴
+  const getUsersList = async () => {
+    const array: any[] = [];
+
+    const q = query(
+      collection(db, 'Users'),
+      where('userEmail', '==', session?.user?.email),
+    );
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) =>
+      array.push({
+        ...doc.data(),
+      }),
+    );
+    setUsersList(array)
+  };
+
+  // 비로그인 유저일 경우 접근 제한
+  useEffect(() => {
+    if (status === "unauthenticated") router.push('/')
+  }, [status])
+  
+  // session(유저 정보)가 들어오면 getUsersList 함수 실행
+  useEffect(() => {
+    if (session) getUsersList();
+  }, [session])
+
+  return (
+    <>
+      <div>닉네임: {usersList[0]?.userName}</div>
+    </>
+  )
 };
 
 export default MyPage;
