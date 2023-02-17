8aad366005ede9d0b01dfe9486ad00929077f668
heeryoung <winterbooks@naver.com>
2023-02-17 23:27:04 +0900

    feat: fullcalender 설치 및 청약캘린더 페이지에 연결

--- src/pages/calender/index.tsx
+++ src/pages/calender/index.tsx
@@ -1,8 +1,28 @@
-import Calendar from 'react-calendar';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
 
 const Calender = () => {
   return (
-    <Calendar />
+    <FullCalendar
+    plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+    headerToolbar={{
+      left: 'prev',
+      center: 'title',
+      right: 'next',
+      // right: 'dayGridMonth,timeGridWeek'
+    }}
+    initialView='dayGridMonth'
+    nowIndicator={true}
+    editable={true}
+    selectable={true}
+    // aspectRatio={2} // 종횡비-너비가 높이의 두 배
+    // height={"90%"} 
+    initialEvents={[
+      { title: 'nice event', start: new Date() }
+    ]}
+  />
   );
 };
  
