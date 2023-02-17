e1cbc330568ab8b1a6b9bb07b5192d5766c2e1d4
hwan910 <psh5575@gmail.com>
2023-02-17 23:46:23 +0900

    design칼렌다 크기 조정

--- src/pages/calender/index.tsx
+++ src/pages/calender/index.tsx
@@ -1,29 +1,29 @@
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import timeGridPlugin from '@fullcalendar/timegrid'
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 const Calender = () => {
   return (
-    <FullCalendar
-    plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
-    headerToolbar={{
-      left: 'prev',
-      center: 'title',
-      right: 'next',
-      // right: 'dayGridMonth,timeGridWeek'
-    }}
-    initialView='dayGridMonth'
-    nowIndicator={true}
-    editable={true}
-    selectable={true}
-    // aspectRatio={2} // 종횡비-너비가 높이의 두 배
-    // height={"90%"} 
-    initialEvents={[
-      { title: 'nice event', start: new Date() }
-    ]}
-  />
+    <div style={{ width: '100vw' }}>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+        headerToolbar={{
+          left: 'prev',
+          center: 'title',
+          right: 'next',
+          // right: 'dayGridMonth,timeGridWeek'
+        }}
+        initialView="dayGridMonth"
+        nowIndicator={true}
+        editable={true}
+        selectable={true}
+        // aspectRatio={2} // 종횡비-너비가 높이의 두 배
+        height={'90%'}
+        initialEvents={[{ title: 'nice event', start: new Date() }]}
+      />
+    </div>
   );
 };
- 
+
 export default Calender;
