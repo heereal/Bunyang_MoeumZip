1f9a4ff5e76f97465b68afbc3fb1d46f1fd60206
heeryoung <winterbooks@naver.com>
2023-02-18 14:44:46 +0900

    feat: 캘린더에 청약 접수 마감일 기준으로 이벤트 불러오기 성공

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -3,8 +3,34 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import * as S from '../../styles/claendar.style';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '@/common/firebase';
+import { useQuery } from 'react-query';
+import { useEffect, useState } from 'react';
 
 const Calender = () => {
+  const [homeList, setHomeList] = useState([]);
+
+  const getHomeList = async () => {
+    const docRef = doc(db, 'HomeList', 'homeData');
+    const docSnap = await getDoc(docRef);
+    return docSnap.data();
+  };
+
+  const { data } = useQuery('TEST', getHomeList);
+
+  const array: any = [];
+  homeList.map((item: any) =>
+    array.push({
+      title: item.HOUSE_NM,
+      date: item.RCEPT_ENDDE,
+    }),
+  );
+
+  useEffect(() => {
+    if (data) setHomeList(data.allHomeData);
+  }, [data]);
+
   return (
     <S.CalendarContainer>
       <FullCalendar
@@ -23,12 +49,13 @@ const Calender = () => {
         // aspectRatio={2} // 종횡비-너비가 높이의 두 배
         // height={"90%"}
         initialEvents={[{ title: 'nice event', start: new Date() }]}
-        events={[
-          {
-            title: '최종프로젝트',
-            date: '2023-02-01',
-          },
-        ]}
+        events={array}
+        // events={[
+        //   {
+        //     title: '최종프로젝트',
+        //     date: '2023-02-01',
+        //   },
+        // ]}
       />
     </S.CalendarContainer>
   );
