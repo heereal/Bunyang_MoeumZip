0d7344498a0ec6eb97bdc585a1bb1c13029eb737
YS739 <suk921@gmail.com>
2023-02-27 11:25:05 +0900

    overlay도 dynamic으로 변경

--- src/components/GlobalComponents/MapSection/Overlay.tsx
+++ src/components/GlobalComponents/MapSection/Overlay.tsx
@@ -1,21 +1,23 @@
 // import { getToday } from '@/common/utils';
 import * as S from './style';
 import { useEffect, useState } from 'react';
+import { getToday } from '@/common/utils';
 
 const Overlay = ({ result }: any) => {
-  const [today, setToday] = useState(Date);
+  // const [today, setToday] = useState(Date);
 
   // 오늘 날짜
-  useEffect(() => {
-    if (window) {
-      const date = new Date();
-      const year = date.getFullYear();
-      const month = ('0' + (date.getMonth() + 1)).slice(-2);
-      const day = ('0' + date.getDate()).slice(-2);
-      const today = year + '-' + month + '-' + day;
-      setToday(today);
-    }
-  }, []);
+  const today = getToday();
+  // useEffect(() => {
+  //   if (window) {
+  //     const date = new Date();
+  //     const year = date.getFullYear();
+  //     const month = ('0' + (date.getMonth() + 1)).slice(-2);
+  //     const day = ('0' + date.getDate()).slice(-2);
+  //     const today = year + '-' + month + '-' + day;
+  //     setToday(today);
+  //   }
+  // }, []);
 
   return (
     <S.OverlayContainer
