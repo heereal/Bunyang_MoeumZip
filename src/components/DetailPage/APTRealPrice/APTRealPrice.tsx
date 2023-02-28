1e7b3a83e544ab2234afafda792ffde81f610c85
heeryoung <winterbooks@naver.com>
2023-03-01 00:46:24 +0900

    feat: 디테일 페이지에서 '동' 기준으로 아파트 매매 실거래가 필터링 구현 LAWD_CD 리스트 배열 -> 객체로 변경, APTRealPrice 컴포넌트 query 추가

--- src/components/DetailPage/APTRealPrice/APTRealPrice.tsx
+++ src/components/DetailPage/APTRealPrice/APTRealPrice.tsx
@@ -2,45 +2,57 @@ import axios from 'axios';
 import * as S from './style';
 import { LAWD_CD_Code } from '@/common/LAWD_CD';
 import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 
 const SERVICE_KEY = process.env.NEXT_PUBLIC_HOME_API_KEY;
 
 const APTRealPrice = ({ detail }: any) => {
-  // LAWD_CD_Code를 객체에서 배열로 변경한 것
-  const [LAWD_CDList, setLAWD_CDList] = useState([]);
+  const [dongList, setDongList] = useState([]);
 
-  const LAWD_CD: any = LAWD_CDList.find(
+  // LAWD_CD_Code에서 현재 페이지 주소에 해당하는 지역 코드 찾기
+  const LAWD_CD: any = LAWD_CD_Code.find(
     (item: string) => item.split(':')[1] === detail.HSSPLY_ADRES.split(' ')[1],
   );
-  console.log('LAWD_CD:', LAWD_CD?.split(':')[0]);
 
+  // 특정 지역의 아파트 매매 실거래가 정보를 가져옴
   const getAPTRealPriceList = async () => {
-    await axios
+    const data = await axios
       .get(
         `/api/APTRealPrice?numOfRows=1000&LAWD_CD=${
           LAWD_CD?.split(':')[0]
-        }&DEAL_YMD=202301&serviceKey=${SERVICE_KEY}`,
+        }&DEAL_YMD=202302&serviceKey=${SERVICE_KEY}`,
       )
-      .then((res) => console.log('성공?', res.data.response.body));
+      .then((res) => res.data.response.body.items.item);
+    return data;
   };
 
-  useEffect(() => {
-    getAPTRealPriceList();
-  }, [LAWD_CD]);
+  const { data } = useQuery('APTRealPriceList', getAPTRealPriceList, {
+    enabled: !!LAWD_CD, // LAWD_CD이 있는 경우에만 useQuery를 실행함
+    // 지역코드로 불러온 아파트 매매 실거래가 리스트에서 '동' 기준으로 필터링하기
+    onSuccess: (data) => {
+      setDongList(
+        data?.filter(
+          (item: any) =>
+            (item.법정동.split(' ')[0] === ''
+              ? item.법정동.split(' ')[1]
+              : item.법정동.split(' ')[0]) ===
+            detail.HSSPLY_ADRES.split(' ')[2],
+        ),
+      );
+    },
+  });
 
-  useEffect(() => {
-    const array: any = [];
-    // object 를 array 로 변경함
-    for (const [key, value] of Object.entries(LAWD_CD_Code)) {
-      array.push(`${key}:${value}`);
-    }
-    setLAWD_CDList(array);
-  }, []);
+  console.log('dongList:', dongList);
 
   return (
     <S.Wrapper>
       <button onClick={getAPTRealPriceList}>✨클릭하세요✨</button>
-      APTRealPrice
+      {dongList?.map((item: any) => (
+        <div key={item.일련번호}>
+          {item.아파트} | {item.법정동}
+          {item.도로명} | 거래금액: {item.거래금액}만원
+        </div>
+      ))}
     </S.Wrapper>
   );
 };
--- src/components/DetailPage/APTRealPrice/style.tsx
+++ src/components/DetailPage/APTRealPrice/style.tsx
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   background-color: white;
   width: 100%;
 `;
