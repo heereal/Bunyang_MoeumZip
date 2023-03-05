23dbc1f651177ae50d33e406a85f9070a9efb644
heeryoung <winterbooks@naver.com>
2023-03-05 22:07:17 +0900

    design: 청약캘린더 분양형태에 따라 이벤트 색상 지정

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,5 +1,4 @@
 import { db } from '@/common/firebase';
-import HeadTitle from '@/components/GlobalComponents/HeadTitle/HeadTitle';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import FullCalendar from '@fullcalendar/react';
@@ -8,47 +7,105 @@ import { GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
 import * as S from '../../styles/claendar.style';
 import { NextSeo } from 'next-seo';
+import Image from 'next/image';
+import Calendar from '../../../public/assets/calendar.png';
 
 const Calender = ({ homeList }: any) => {
   const router = useRouter();
 
-  // 캘린더에 이벤트를 보여주기 위해 기존 분양 데이터를 재가공함
   const array: any = [];
+  console.log('homeList:', homeList);
+
+  // 캘린더에 이벤트를 보여주기 위해 기존 분양 데이터를 재가공함
   homeList.allHomeData.map((item: any) =>
     array.push({
       title: item.HOUSE_NM,
       date: item.RCEPT_ENDDE,
       id: item.PBLANC_NO,
+      borderColor: 'transparent',
+      backgroundColor:
+        item.HOUSE_DTL_SECD_NM === '행복주택' ||
+        item.HOUSE_DTL_SECD_NM === '공공임대' ||
+        item.HOUSE_DTL_SECD_NM === '국민임대' ||
+        item.HOUSE_DTL_SECD_NM === '영구임대' ||
+        item.HOUSE_DTL_SECD_NM === '민간임대' ||
+        item.HOUSE_DTL_SECD_NM === '공공지원민간임대'
+          ? '#EFF4FF'
+          : item.HOUSE_DTL_SECD_NM === '분양주택' ||
+            item.HOUSE_DTL_SECD_NM === '민영' ||
+            item.HOUSE_DTL_SECD_NM === '국민'
+          ? '#FFECEC'
+          : item.HOUSE_DTL_SECD_NM === '신혼희망타운'
+          ? '#F9EDFF'
+          : item.HOUSE_DTL_SECD_NM === '도시형생활주택'
+          ? '#FFFCE0'
+          : item.HOUSE_DTL_SECD_NM === '계약취소'
+          ? '#E8FFF2'
+          : '#b1b7bdac',
+
+      textColor:
+        item.HOUSE_DTL_SECD_NM === '행복주택' ||
+        item.HOUSE_DTL_SECD_NM === '공공임대' ||
+        item.HOUSE_DTL_SECD_NM === '국민임대' ||
+        item.HOUSE_DTL_SECD_NM === '영구임대' ||
+        item.HOUSE_DTL_SECD_NM === '민간임대' ||
+        item.HOUSE_DTL_SECD_NM === '공공지원민간임대'
+          ? '#356EFF'
+          : item.HOUSE_DTL_SECD_NM === '분양주택' ||
+            item.HOUSE_DTL_SECD_NM === '민영' ||
+            item.HOUSE_DTL_SECD_NM === '국민'
+          ? '#FF4141'
+          : item.HOUSE_DTL_SECD_NM === '신혼희망타운'
+          ? '#AF1DF3': item.HOUSE_DTL_SECD_NM === '도시형생활주택'
+          ? '#D4BF00'
+          : item.HOUSE_DTL_SECD_NM === '계약취소'
+          ? '#24CE71'
+          : 'white',
     }),
   );
 
   return (
-    <S.CalendarContainer>
+    <S.CalendarWrapper>
       <NextSeo
         title="청약캘린더 -"
         description="청약일정을 캘린더에서 한 눈에 확인 가능합니다."
       />
-
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        headerToolbar={{
-          left: 'prev',
-          center: 'title',
-          right: 'next',
-        }}
-        initialView="dayGridMonth"
-        nowIndicator={true}
-        selectable={true}
-        locale={'ko'} // 한글 표기
-        // aspectRatio={1} // 종횡비-너비가 높이의 두 배
-        // height={"800"}
-        events={array}
-        // eventDisplay={'list-item'}
-        eventColor="#6096B4"
-        // eventMouseEnter={(e)=>e.target.style={}}
-        eventClick={(e) => router.push(`/detail/${e.event.id}`)}
-      />
-    </S.CalendarContainer>
+      <S.CalendarHeader>
+        <S.CalendarDescContainer>
+          청약 일정을 확인해 보세요.
+        </S.CalendarDescContainer>
+        <S.CalendarIcon>
+          <Image
+            src={Calendar}
+            alt="spinner"
+            height={50}
+            quality={100}
+            priority={true}
+          />
+        </S.CalendarIcon>
+      </S.CalendarHeader>
+      <S.CalendarContainer>
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: 'prev',
+            center: 'title',
+            right: 'next',
+          }}
+          initialView="dayGridMonth"
+          nowIndicator={true}
+          selectable={true}
+          locale={'ko'} // 한글 표기
+          // aspectRatio={1} // 종횡비-너비가 높이의 두 배
+          // height={"800"}
+          events={array}
+          // eventDisplay={'list-item'}
+          // eventColor="#6096B4"
+          // eventMouseEnter={(e)=>e.target.style={}}
+          eventClick={(e) => router.push(`/detail/${e.event.id}`)}
+        />
+      </S.CalendarContainer>
+    </S.CalendarWrapper>
   );
 };
 
--- src/styles/claendar.style.tsx
+++ src/styles/claendar.style.tsx
@@ -1,21 +1,46 @@
 import styled from 'styled-components';
 
+export const CalendarWrapper = styled.div`
+  width: 100%;
+  height: 900px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px 0;
+  box-sizing: content-box;
+`;
+
+export const CalendarHeader = styled.div`
+  width: 100%;
+  padding: 0 40px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const CalendarDescContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CalendarIcon = styled.div``;
+
 export const CalendarContainer = styled.div`
-  width: 100vw;
-  height: 800px;
+  width: 80%;
+  /* width: 940px; */
+  height: 100%;
+  /* height: 800px; */
   display: flex;
   justify-content: center;
-  padding: 50px 0;
-  box-sizing: content-box;
-  /* display: inline-block; */
+  /* padding: 40px 0; */
+  /* box-sizing: content-box; */
 
   // 캘린더 전체 사이즈 조정
   .fc {
-    width: 65%;
+    width: 100%;
   }
 
   // 각 이벤트 요소
-  .fc-event{
+  .fc-event {
     cursor: pointer;
     padding: 4px 6px 6px 6px;
     margin-bottom: 2px;
