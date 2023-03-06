ca611021daed49e4d7082ac8fb1265f266f39cbc
hwan910 <psh5575@gmail.com>
2023-03-06 16:46:20 +0900

    edit:오지 이미지 5차 수정

--- seo.config.js
+++ seo.config.js
@@ -7,14 +7,14 @@ export default {
     images: [
       {
         //TODO: 배포하면 주소 바꿔야함
-        url: 'https://media.discordapp.net/attachments/1071956935471009888/1082204623651225630/2023-03-06__16.33.50.png',
+        url: 'https://media.discordapp.net/attachments/1071956935471009888/1082207188782698517/oggg.png',
       },
     ],
   },
   additionalLinkTags: [
     {
       rel: 'ogImage',
-      href: 'https://media.discordapp.net/attachments/1071956935471009888/1082204623651225630/2023-03-06__16.33.50.png',
+      href: 'https://media.discordapp.net/attachments/1071956935471009888/1082207188782698517/oggg.png',
     },
   ],
 };
