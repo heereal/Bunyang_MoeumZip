c145cf75a9172a3b2ce34dff363f138dff3d8eed
hwan910 <psh5575@gmail.com>
2023-03-06 13:34:13 +0900

    파비콘 / og태그 변경

--- seo.config.js
+++ seo.config.js
@@ -14,7 +14,7 @@ export default {
   additionalLinkTags: [
     {
       rel: 'favicon',
-      href: 'https://by-dev.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffavicon.e7de0244.ico&w=48&q=75',
+      href: 'https://media.discordapp.net/attachments/1071986566014177360/1081946338364755998/50ce22bcdbd576e8.png?width=1134&height=322',
     },
   ],
 };
