562e89f23c332d2de15c4363771d2af02cad5041
heeryoung <winterbooks@naver.com>
2023-03-06 13:18:35 +0900

    feat: 청약캘린더 데이터 재가공까지 SSG 적용

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -15,12 +15,73 @@ import { calendarColorList } from '@/common/categoryList';
 const Calender = ({ homeList }: any) => {
   const router = useRouter();
 
-  const array: any = [];
-  console.log('homeList:', homeList);
+  return (
+    <>
+      <S.CalendarWrapper>
+        <NextSeo
+          title="청약캘린더 -"
+          description="청약일정을 캘린더에서 한 눈에 확인 가능합니다."
+        />
+        <S.CalendarContainer>
+          <S.CalendarHeader>
+            <S.CalendarDescContainer>
+              <S.Title>청약 일정을 확인해 보세요.</S.Title>
+              <S.CategoryContainer>
+                {calendarColorList.map((item, index) => (
+                  <S.Category key={index} color={item.color}>
+                    <RxDotFilled size="20" style={{ marginRight: 2 }} />
+                    {item.cate}
+                  </S.Category>
+                ))}
+              </S.CategoryContainer>
+            </S.CalendarDescContainer>
+            <S.CalendarIcon>
+              <Image
+                className="calendarIcon"
+                src={Calendar}
+                alt="calendarIcon"
+                height={80}
+                quality={100}
+                priority={true}
+              />
+            </S.CalendarIcon>
+          </S.CalendarHeader>
+          <S.FullCalendarContainer>
+            <FullCalendar
+              plugins={[dayGridPlugin, interactionPlugin]}
+              headerToolbar={{
+                left: 'prev',
+                center: 'title',
+                right: 'next',
+              }}
+              initialView="dayGridMonth"
+              nowIndicator={true}
+              fixedWeekCount={false} // 달에 따라 4-6주를 보여줌 (6주로 고정x)
+              weekends={false} // 토요일 일요일 제거
+              locale={'ko'} // 한글 표기
+              contentHeight="auto" // 스크롤 생성되지 않고 높이 자동 조절
+              events={homeList}
+              eventClick={(e) => router.push(`/detail/${e.event.id}`)}
+            />
+          </S.FullCalendarContainer>
+        </S.CalendarContainer>
+        <S.HeaderBackground />
+      </S.CalendarWrapper>
+    </>
+  );
+};
 
+export default Calender;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const docRef = doc(db, 'HomeList', 'homeData');
+  const docSnap = await getDoc(docRef);
+  const getHomeList = docSnap.data();
+
+  const homeList: any = [];
   // 캘린더에 이벤트를 보여주기 위해 기존 분양 데이터를 재가공함
-  homeList.allHomeData.map((item: any) =>
-    array.push({
+  getHomeList?.allHomeData.map((item: any) =>
+    homeList.push({
       title: item.HOUSE_NM,
       date: item.RCEPT_ENDDE,
       id: item.PBLANC_NO,
@@ -68,70 +129,6 @@ const Calender = ({ homeList }: any) => {
           : 'white',
     }),
   );
-
-  return (
-    <>
-      <S.CalendarWrapper>
-        <NextSeo
-          title="청약캘린더 -"
-          description="청약일정을 캘린더에서 한 눈에 확인 가능합니다."
-        />
-        <S.CalendarContainer>
-          <S.CalendarHeader>
-            <S.CalendarDescContainer>
-              <S.Title>청약 일정을 확인해 보세요.</S.Title>
-              <S.CategoryContainer>
-                {calendarColorList.map((item, index) => (
-                  <S.Category key={index} color={item.color}>
-                    <RxDotFilled size="20" style={{ marginRight: 2 }} />
-                    {item.cate}
-                  </S.Category>
-                ))}
-              </S.CategoryContainer>
-            </S.CalendarDescContainer>
-            <S.CalendarIcon>
-              <Image
-                className="calendarIcon"
-                src={Calendar}
-                alt="calendarIcon"
-                height={80}
-                quality={100}
-                priority={true}
-              />
-            </S.CalendarIcon>
-          </S.CalendarHeader>
-          <S.FullCalendarContainer>
-            <FullCalendar
-              plugins={[dayGridPlugin, interactionPlugin]}
-              headerToolbar={{
-                left: 'prev',
-                center: 'title',
-                right: 'next',
-              }}
-              initialView="dayGridMonth"
-              nowIndicator={true}
-              fixedWeekCount={false} // 매달에 따라 4-6주를 보여줌 (6주로 고정x)
-              weekends={false} // 토요일 일요일 제거
-              locale={'ko'} // 한글 표기
-              contentHeight="auto" // 스크롤 생성되지 않고 높이 자동 조절
-              events={array}
-              // eventMouseEnter={(e)=>e.target.style={}}
-              eventClick={(e) => router.push(`/detail/${e.event.id}`)}
-            />
-          </S.FullCalendarContainer>
-        </S.CalendarContainer>
-        <S.HeaderBackground />
-      </S.CalendarWrapper>
-    </>
-  );
-};
-
-export default Calender;
-
-export const getStaticProps: GetStaticProps = async () => {
-  const docRef = doc(db, 'HomeList', 'homeData');
-  const docSnap = await getDoc(docRef);
-  const homeList = docSnap.data();
 
   return {
     props: { homeList },
