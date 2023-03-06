54a57925f0c2d86404862f3b9bdcd2e13a991204
hwan910 <psh5575@gmail.com>
2023-03-06 16:36:30 +0900

    edit:로딩 페이지 코드 수정

--- seo.config.js
+++ seo.config.js
@@ -7,14 +7,14 @@ export default {
     images: [
       {
         //TODO: 배포하면 주소 바꿔야함
-        url: 'https://media.discordapp.net/attachments/1071986566014177360/1081946338364755998/50ce22bcdbd576e8.png?width=810&height=230',
+        url: 'https://media.discordapp.net/attachments/1071956935471009888/1082204623651225630/2023-03-06__16.33.50.png',
       },
     ],
   },
   additionalLinkTags: [
     {
       rel: 'ogImage',
-      href: 'https://firebasestorage.googleapis.com/v0/b/bunyang-moa.appspot.com/o/profileImages%2F89a682e9-1a82-4bd3-987c-6b5723ad88b7?alt=media&token=dc719e12-6df1-472a-978a-19368a90f977',
+      href: 'https://media.discordapp.net/attachments/1071956935471009888/1082204623651225630/2023-03-06__16.33.50.png',
     },
   ],
 };
