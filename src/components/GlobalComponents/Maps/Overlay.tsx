dbe26120056d2dda9c6d66f93900e0190b9d3272
hwan910 <psh5575@gmail.com>
2023-03-09 16:37:15 +0900

    design: 반응형 메인페이지 카드 완전 안보이게 바꿈, 상세페이지 갔다가 메인페이지 돌아와도 지도 줌 다시 안땡겨지게했음

--- src/components/GlobalComponents/Maps/Overlay.tsx
+++ src/components/GlobalComponents/Maps/Overlay.tsx
@@ -9,15 +9,16 @@ const Overlay = ({ result }: any) => {
       <div style={{ fontWeight: 600 }}>{result?.HOUSE_NM}</div>
 
       <div>
-        {!result?.MIN_LTTOT_TOP_AMOUNT
-          ? '모집공고문 확인'
-          : result?.MIN_LTTOT_TOP_AMOUNT === result?.MAX_LTTOT_TOP_AMOUNT
-          ? result?.MAX_LTTOT_TOP_AMOUNT
-          : result?.MIN_LTTOT_TOP_AMOUNT.includes('만원')
-          ? result?.MIN_LTTOT_TOP_AMOUNT.slice(0, -2) +
+        {result?.RCEPT_BGNDE &&
+          result?.RCEPT_BGNDE.slice(5, 7).replace(/(^0)/, '') +
+            '월 ' +
+            result?.RCEPT_BGNDE.slice(8, 10).replace(/(^0)/, '') +
+            '일' +
             ' ~ ' +
-            result?.MAX_LTTOT_TOP_AMOUNT
-          : result?.MIN_LTTOT_TOP_AMOUNT + ' ~ ' + result?.MAX_LTTOT_TOP_AMOUNT}
+            result?.RCEPT_ENDDE.slice(5, 7).replace(/(^0)/, '') +
+            '월 ' +
+            result?.RCEPT_ENDDE.slice(8, 10).replace(/(^0)/, '') +
+            '일'}
       </div>
     </S.OverlayContainer>
   );
