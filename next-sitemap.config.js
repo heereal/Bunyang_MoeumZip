5b4b158025ef5090ad47a22abe8da9e05e40d4a5
heeryoung <winterbooks@naver.com>
2023-06-05 15:08:19 +0900

    edit: 모달 ui 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,13 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://www.by-zip.com',
     generateRobotsTxt: true, // (optional)
-    // ...other options
-  }
+    robotsTxtOptions: { // robot.txt 옵션 (필요시 작성)
+      policies: [
+        { userAgent: '*', allow: '/' }, // 접근 허용
+        {
+          userAgent: '*',
+          disallow: ['/404', '/my', '/admin', '/signup', '/loading'],  // 접근 비허용
+        },
+      ],
+    },
+}
